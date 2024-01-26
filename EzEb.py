3b489d86f735b830e5c2e5510b60c22f4bf6e614
jeremys.social <71183843+jeremyssocial@users.noreply.github.com>
2024-01-26 09:19:47 +0100

    Refactored script for readability and efficiency; removed unnecessary os-sys import, added error handling, and optimized file operations for improved user experience.

--- EzEb.py
+++ EzEb.py
@@ -2,77 +2,68 @@ import os
 import cv2
 import glob
 from PIL import Image
-import numpy
-import sys
 
+# Set the current directory to the script's directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-os.chdir(sys.path[0])
-
+# Process video
 vidname = os.listdir("./video")
-print(vidname)
-os.rename("./video/"+vidname[0],"./video/vid.mp4")
+if vidname:
+    os.rename(f"./video/{vidname[0]}", "./video/vid.mp4")
 
-cam = cv2.VideoCapture("./video/vid.mp4")
-currentframe = 0
-while(True):
-    ret,frame = cam.read()
-
-    if ret:
-        name = str("./frames/"+str(currentframe).zfill(4)+".jpg")
-        print("Frame "+ str(currentframe))
-        cv2.imwrite(name, frame)
-        currentframe += 1
-    else:
-        break
-cam.release() 
-cv2.destroyAllWindows() 
-
-
-print("nice, now it's time for style.")
-more = True
-while(more):
-    print("if you already have an image in the styleOriginal folder, please remove it.")
-    input("then put ONE style picture in the styleOriginal folder and hit enter")
-    styleframe = str(input("Which frame is your style picture now? (enter the frame number) "))
-    imgname = glob.glob("./styleOriginal/*")
-    os.rename(str(imgname[0]),"./styleOriginal/"+styleframe+".jpg")
-    imgname = glob.glob("./styleOriginal/*")
-
-    img = Image.open("./frames/"+str(styleframe)+".jpg")
-    size = img.size
-    im = Image.open(str(imgname[0]))
-    im_resized = im.resize(size, Image.ANTIALIAS)
-    im_resized.save("./styleCorrect/"+str(styleframe)+".png", "PNG")
-    im.close()
-
-    keepasking = True
-    while(keepasking):
-        yesORno = str(input("Do you have another one? (y / n)"))
-        if yesORno == "y":
-            more = True
-            keepasking = False
-        elif yesORno == "n":
-            more = False
-            keepasking = False
+    cam = cv2.VideoCapture("./video/vid.mp4")
+    currentframe = 0
+    while True:
+        ret, frame = cam.read()
+        if ret:
+            name = f"./frames/{str(currentframe).zfill(4)}.jpg"
+            print(f"Frame {currentframe}")
+            cv2.imwrite(name, frame)
+            currentframe += 1
         else:
-            print("either y or n")
+            break
+    cam.release()
+    cv2.destroyAllWindows()
+
+print("Nice, now it's time for style.")
+
+# Style application loop
+more = True
+while more:
+    print("If you already have an image in the styleOriginal folder, please remove it.")
+    input("Then put ONE style picture in the styleOriginal folder and hit enter")
+    styleframe = input("Which frame is your style picture now? (enter the frame number): ")
+    imgname = glob.glob("./styleOriginal/*")
+    if imgname:
+        os.rename(imgname[0], f"./styleOriginal/{styleframe}.jpg")
+
+    try:
+        img = Image.open(f"./frames/{styleframe}.jpg")
+        im = Image.open(f"./styleOriginal/{styleframe}.jpg")
+        im_resized = im.resize(img.size, Image.ANTIALIAS)
+        im_resized.save(f"./styleCorrect/{styleframe}.png", "PNG")
+        im.close()
+    except IOError as e:
+        print(f"Error processing images: {e}")
+
+    while True:
+        yes_or_no = input("Do you have another one? (y / n): ").lower()
+        if yes_or_no in ["y", "n"]:
+            more = yes_or_no == "y"
+            break
+        else:
+            print("Either 'y' or 'n'")
 
 input("Do what you have to do in EbSynth, then return here and press enter")
 
-#see how many fps the video called "vid.mp4" in the folder "./video" has and save it to a variable called "fps"
+# Video properties
 vidcap = cv2.VideoCapture("./video/vid.mp4")
 fps = vidcap.get(cv2.CAP_PROP_FPS)
-
-#see the height and width of the video called "vid.mp4" in the folder "./video" and save them to the variables "width" and "height"
 width = vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)
 height = vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-#turn all the frames saved in the folder "./render" into a video called "result.mp4" in the directory "./result". The fps of the video should be the variable "fps" and the width and height should be the variables "width" and "height".
-#check if the images in the folder "./render" are png or jpg
-#if the images are png use os.system("ffmpeg -r "+str(fps)+" -i ./render/%04d.png -vcodec mpeg4 -y ./result/result.mp4")
-#if the images are jpg use os.system("ffmpeg -r "+str(fps)+" -i ./render/%04d.jpg -vcodec mpeg4 -y ./result/result.mp4")
-
+# Create final video
 if os.path.exists("./render/0001.png"):
-    os.system("ffmpeg -r "+str(fps)+" -i ./render/%04d.png -vcodec mpeg4 -y ./result/result.mp4")
+    os.system(f"ffmpeg -r {fps} -i ./render/%04d.png -vcodec mpeg4 -y ./result/result.mp4")
 elif os.path.exists("./render/0001.jpg"):
-    os.system("ffmpeg -r "+str(fps)+" -i ./render/%04d.jpg -vcodec mpeg4 -y ./result/result.mp4")
+    os.system(f"ffmpeg -r {fps} -i ./render/%04d.jpg -vcodec mpeg4 -y ./result/result.mp4")
